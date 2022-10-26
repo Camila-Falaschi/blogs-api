82b58d2773d3ee9b9dec1a3b8a20e7694cdada59
Camila <falaschi.camila@yahoo.com.br>
2022-10-26 16:24:25 -0300

    creating category model with sequelize.define

---
+++ src/models/Category.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  },
+  { tableName: 'categories' });
+
+  return Category;
+};
